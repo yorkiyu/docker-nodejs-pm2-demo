c9735398b0f9402d8492e2670702fdfcdaa134ab
yorkyu <yorkyu@tencent.com>
2020-11-08 11:57:52 +0800

    fix: node server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 
 http.createServer((request, response) => {
-    return 'Docker-Nodejs-PM2-Demo, Hello World!';
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Docker-nodejs-pm2-demo, Hello World!');
 }).listen(8080);
